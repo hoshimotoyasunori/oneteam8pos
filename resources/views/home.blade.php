0adf8ea20b96ddc0a8dc28e6491e2728ba07f8d7
hoshimoto <hoshimotoyasunori@gmail.com>
2021-04-17 06:38:39 +0000

    12

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged  on in!
                 </div>
             </div>
         </div>
