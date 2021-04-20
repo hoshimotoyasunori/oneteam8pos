7c2984db450548df089d6c98f1d3cd41dce53e20
hoshimoto <hoshimotoyasunori@gmail.com>
2021-04-20 14:13:36 +0000

    navbar-list

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,10 @@
   </head>
 
   <body>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>-->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     <header class="sticky-top">
       @yield('navbar')
     </header>
@@ -29,9 +33,9 @@
        </div>
        
        @yield('footer')
-       <!-- JQuery -->
-　　　　<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      // <!-- JQuery -->
+　　　　<!--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>-->
+    <!--    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>-->
 
   </body>
 </html>
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,21 +1,24 @@
 @section('navbar')
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container  bg-light">
-        <a class="navbar__brand navbar__mainLogo" href="/"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-md-auto align-items-center">
-            <li>
-              <a class="btn btn-primary" href="/posts/new">投稿</a>
+    <nav class=" navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar__brand navbar__mainLogo" href="#"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <div class="collapse navbar-collapse bg-light text-right" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="/users/{{ Auth::user()->id }}">マイページ</a>
             </li>
-            <li>
-              <a class="nav-link commonNavIcon profile-icon" href="/users/{{ Auth::user()->id }}">
-              </a>マイページ
+            <li class="nav-item active">
+             <a class="nav-link " href="/posts/new">画像投稿</a>
             </li>
-          </ul>
-        </div>
+            <li class="nav-item active">
+              <a class="nav-link" rel="nofollow" data-method="POST" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+            </li>
+        </ul>
       </div>
+      
     </nav>
 @endsection
