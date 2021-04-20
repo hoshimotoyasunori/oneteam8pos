28837dc76c339f6f7026750f8c5e8d4c9d5500d0
hoshimoto <hoshimotoyasunori@gmail.com>
2021-04-20 12:27:47 +0000

    0420-1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,19 +11,27 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <!-- Styles -->
     <link href="{{ secure_asset('css/application.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('js/app.js') }}" rel="stylesheet">
-    
+    <!--<link href="{{ secure_asset('js/app.js') }}" rel="stylesheet">-->
+    <!--<link href="css/carousel.css" rel="stylesheet">-->
+ 
   </head>
 
   <body>
-       @yield('navbar')
+    <header class="sticky-top">
+      @yield('navbar')
+    </header>
        
-       <div class="container">
+       
+       <div class="container p-1">
          <!--<div class="row">-->
             @yield('content')
         <!--</div>-->
        </div>
        
        @yield('footer')
+       <!-- JQuery -->
+　　　　<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
   </body>
 </html>
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,22 +1,21 @@
-
 @section('navbar')
-    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
-      <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container  bg-light">
         <a class="navbar__brand navbar__mainLogo" href="/"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <nav class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-md-auto align-items-center">
             <li>
               <a class="btn btn-primary" href="/posts/new">投稿</a>
             </li>
             <li>
               <a class="nav-link commonNavIcon profile-icon" href="/users/{{ Auth::user()->id }}">
-              </a>
+              </a>マイページ
             </li>
           </ul>
-        </nav>
+        </div>
       </div>
     </nav>
 @endsection
