01d6a2be46c083e94b92b57149dfd2fd78a5c4ab
hoshimoto <hoshimotoyasunori@gmail.com>
2021-04-22 13:59:15 +0000

    profi-ru

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -9,7 +9,7 @@
       <div class="col-md-4 text-center">
         @if ($user->profile_photo)
           <p>
-            <img class="round-img" src="{{ asset('storage/user_images/' . $user->profile_photo) }}"alt="avatar" />
+            <img class="round-img" src="data:image/png;base64,{{ $user->image }}"/>
           </p>
           @else
             <img class="round-img" src="{{ asset('/images/blank_profile.png') }}"/>
