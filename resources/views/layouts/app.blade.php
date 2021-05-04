64a5ae2876ceb9d0dcaf7331c79349206deb6297
hoshimoto <hoshimotoyasunori@gmail.com>
2021-05-04 04:20:38 +0000

    0504

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,17 +17,17 @@
 
   </head>
 
-  <body class="vw-100">
+  <body class="w-100">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous">
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
 
-    <header class="sticky-top container-fuild vw-100">
+    <header class="sticky-top container-fuild w-75">
       @yield('navbar')
     </header>
     <!--// <main>-->
-      <div class="container-fuild px-5 vw-100">
+      <div class="container-fuild px-5 w-75">
          
             @yield('content')
         
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -4,41 +4,38 @@
 
 @section('content')
 <!--<div class="container">-->
-<div class="row">
-        <div class="col-md-1 p-0"></div>
+    <div class="row">
         <div class="col-md-10 p-0">
             <h4>{{ $post->caption }}</h4>
             <string class="light-color post-time no-text-decoration " href="/posts/{{ $post->id }}">{{ $post->created_at }}</string>
-            <span><p>{{ $post->details }}</p></span>
+            <span><p class="m-0">{{ $post->details }}</p></span>
         </div>
-        <div class="col-md-1 p-0"></div>
     </div>
-  <div class="row   pb-5">
-    <div  class="col-md-1 p-0"></div>
+    <div class="row   pb-5">
 <!--/////画像データ/////-->
-    <div class="col-md-8 text-left p-0">
-        <img class="card-img-top embed-responsive-item object-fit: cover;  border border-dark rounded" src="data:image/png;base64,{{ $post->image }}"/>
-    </div>
-<!--/////コメントBOX/////-->
-    <div class="col-md-2 border border-dark rounded ">
-    <!--/////コメント送信欄/////-->
-        <div class="row actions border-bottom border-dark" id="comment-form-post-{{ $post->id }}">
-       	  <form class="w-100" id="new_comment" action="/posts/{{ $post->id }}/comments" accept-charset="UTF-8" data-remote="true" method="post">
-       	    <input name="utf8" type="hidden" value="&#x2713;" />
-         	  {{csrf_field()}} 
-            <input value="{{ Auth::user()->id }}" type="hidden" name="user_id" />
-            <input value="{{ $post->id }}" type="hidden" name="post_id" />
-            <input class="form-control comment-input border-0" placeholder="コメント ..." autocomplete="off" type="text" name="comment" />
-          </form>
+        <div class="col-md-8 text-left p-0">
+            <img class="card-img-top embed-responsive-item object-fit: cover;  border border-dark rounded" src="data:image/png;base64,{{ $post->image }}"/>
         </div>
+<!--/////コメントBOX/////-->
+        <div class="col-md-4 border border-dark rounded px-3">
+        <!--/////コメント送信欄/////-->
+            <div class="row actions border-bottom border-dark" id="comment-form-post-{{ $post->id }}">
+           	  <form class="w-100" id="new_comment" action="/posts/{{ $post->id }}/comments" accept-charset="UTF-8" data-remote="true" method="post">
+           	    <input name="utf8" type="hidden" value="&#x2713;" />
+             	  {{csrf_field()}} 
+                <input value="{{ Auth::user()->id }}" type="hidden" name="user_id" />
+                <input value="{{ $post->id }}" type="hidden" name="post_id" />
+                <input class="form-control comment-input border-0" placeholder="コメント ..." autocomplete="off" type="text" name="comment" />
+              </form>
+            </div>
     <!--/////コメント一覧/////-->
-        <div class="overflow-auto" >
-          <div id="comment-post-{{ $post->id }}">
-            @include('post.comment_list')
-          </div>
-        </div>  
-    </div>
-    <div  class="col-md-1 p-0"></div>
+            <div class="overflow-auto" >
+              <div id="comment-post-{{ $post->id }}">
+                @include('post.comment_list')
+              </div>
+            </div>  
+        </div>
+    </div>    
 </div>
     
   <!--<div class="row">-->
